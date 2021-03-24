ac8ab14ad38dbbe3c6b64d6a0a36fdd8cdbdb349
Marcelo Matos <harley_ctba@hotmail.com>
2021-03-23 21:18:11 -0300

    Initial commit

--- market.py
+++ market.py
@@ -13,6 +13,9 @@ class Item(db.Model):
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     descricao = db.Column(db.String(length=1024), nullable=False, unique=True)
 
+    def __repr__(self):
+        return f'Item{self.name}'
+
 
 @app.route('/')
 @app.route('/index')
@@ -22,9 +25,5 @@ def pagina_inicial():
 
 @app.route('/loja')
 def loja():
-    items = [
-        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-    ]
+    items = Item.query.all()
     return render_template('loja.html', itens=items)
