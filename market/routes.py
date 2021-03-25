936526272860a32528c860297d3a5577cec695a9
Marcelo Matos <harley_ctba@hotmail.com>
2021-03-25 16:03:05 -0300

    Initial commit

--- market/routes.py
+++ market/routes.py
@@ -1,6 +1,7 @@
 from market import app
 from flask import render_template
 from market.models import Item
+from market.forms import RegisterForm
 
 
 @app.route('/')
@@ -13,3 +14,8 @@ def pagina_inicial():
 def loja():
     items = Item.query.all()
     return render_template('loja.html', itens=items)
+
+@app.route('/register')
+def register_page():
+    form = RegisterForm()
+    return render_template('register.html', form=form)
